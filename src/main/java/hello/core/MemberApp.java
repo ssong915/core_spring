70920fffdc1b5b86412ea1818f4e001166433d4f
ssong <yysk_915@naver.com>
2021-10-27 17:25:13 +0900

    [Add] 스프링으로 전환하기

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,13 +4,21 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) { // psvm + enter
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService()
+// -> 기존
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //-> AppConfig 에 있는 환경설정 정보를 가지고, 스프링컨테이너에 넣어서 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+        //-> appConfig의 이름: "memberService", 타입: MemberService.class 을 찾을거야
+
         Member member = new Member(1L, "memberA", Grade.VIP); // ctrl + alt + v
         memberService.join(member); // 가입!
 
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,16 +7,24 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) { //psvm + enter
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
 
-//        MemberService memberService = new MemberServiceImpl(null);
-//        OrderService orderService = new OrderServiceImpl(null,null);
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //-> AppConfig 에 있는 환경설정 정보를 가지고, 스프링컨테이너에 넣어서 관리해줌
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+        //-> appConfig의 이름: "memberService", 타입: MemberService.class 을 찾을거야
+        OrderService orderService = applicationContext.getBean("orderService",OrderService.class);
+        //-> appConfig의 이름: "orderService", 타입: OrderService.class 을 찾을거야
+
 
         // VIP 회원 하나 가입!
         Long memberId = 1L;
@@ -29,7 +37,7 @@ public class OrderApp {
         System.out.println("order = " + order);
         //아까 말한 toString()이 실행될 것
         System.out.println("order = " + order.calculatePrice());
-        // 아까 만든 itemPrice - discoutPrice
+        // 아까 만든 itemPrice - discountPrice
     }
 
 }
