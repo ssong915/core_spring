02b6f7676887f5f34ee4e578b9eb7f5cd5c10d67
ssong <yysk_915@naver.com>
2021-11-05 02:26:04 +0900

    [Add] 다양한 설정 형식 지원 - 자바 코드, XML

--- src/test/java/hello/core/beanfind/ApplicationContextExtendFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendFindTest.java
@@ -56,7 +56,7 @@ class ApplicationContextExtendsFindTest {
     @DisplayName("부모 타입으로 모두 조회하기 - Object")
     void findAllBeanByObjectType() {
         Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
-        
+
         for (String key : beansOfType.keySet()) {
             System.out.println("key = " + key + " value=" + beansOfType.get(key));
         }
